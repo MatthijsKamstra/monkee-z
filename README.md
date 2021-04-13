7f45f05a7a6af7efd8ab89c4dda7ad0738d49ebb
Matthijs Kamstra <matthijs@fonk.amsterdam>
2021-04-13 13:02:37 +0200

    updating

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # monkee-z
-its not alpha, not bravo, but zulu ... the last library you should use
+
+It;s not alpha, not bravo, but zulu ... the last library you should use (and my personal playground)
+
+# 🐵 Monkee Load
+
+simple way to load templates with vanille js.
+
+```
++ docs
+	+ components
+		- nav.html
+		- main.html
+		- footer.html
+```
+
+for example `nav.html` is: `<nav>navigation</nav>`
+
+```html
+<div data-load="components/nav.html"></div>
+<div data-load="components/main.html"></div>
+<div data-load="components/footer.html"></div>
+```
+
+it will load the file `components/nav.html` and when its done, it will replace the div with the content
+
+So `<div data-load="components/nav.html"></div>` will become `<nav>navigation</nav>`.
+
+## Monkee react
+
+I was looking for a simplistic react code that would "repeat" what was said in an input field.
+
+And thought I would try to build one myself.
+
+Spend not long on it (it works) and the I read an article on stimulus. That does exactly what I wanted.
+
+And it has a version 2... so BETTER then mine.
+
+So just a POC that is totally unrelavant at this moment
+
+**stimulus** https://stimulus.hotwire.dev/
---
+++ src/MonkeeReact.hx
@@ -0,0 +1,43 @@
+package;
+
+import js.Browser.*;
+import js.Browser;
+import js.html.*;
+
+class MonkeeReact {
+	public function new() {
+		document.addEventListener('DOMContentLoaded', (event) -> {
+			trace('MonkeeReact');
+		});
+
+		var arr = document.querySelectorAll('[data-wrapper]');
+		for (i in 0...arr.length) {
+			var wrapper:Element = cast arr[i];
+			var wrapperName = (wrapper.getAttribute('data-wrapper'));
+			// get all data-in from wrapper
+			var dataOutArr = wrapper.querySelectorAll('[data-out]');
+			// for (j in 0...dataOutArr.length) {
+			// 	var el = dataOutArr[j];
+			// 	trace(el);
+			// }
+			// get all data-out from wrapper
+			var dataInArr = wrapper.querySelectorAll('[data-in]');
+			for (j in 0...dataInArr.length) {
+				var el:Element = cast dataInArr[j];
+				el.onkeypress = function(e:KeyboardEvent) {
+					var key = (e.key);
+					var value = untyped e.target.value; // cast will add to much haxe stuff to the output
+					var total = '${value}${key}';
+					for (i in 0...dataOutArr.length) {
+						var el:Element = cast dataOutArr[i];
+						el.innerText = total;
+					}
+				}
+			}
+		}
+	}
+
+	static public function main() {
+		var app = new MonkeeReact();
+	}
+}
