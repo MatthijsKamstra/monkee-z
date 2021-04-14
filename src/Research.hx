55947fe87bf5860a0652fe0be79398bf4db1fa44
Matthijs Kamstra <matthijs@fonk.amsterdam>
2021-04-14 11:10:02 +0200

    json and json arrays

--- src/Research.hx
+++ src/Research.hx
@@ -6,26 +6,62 @@ using StringTools;
 using Lambda;
 
 class Research {
-	public function new() {
-		trace('Research');
-		var json = {
-			"i": {
-				"want": {
-					"it": "Oh I know you do..."
-				}
+	// json
+	var json = {
+		"i": {
+			"want": {
+				"it": "Oh I know you do...",
+				"to": {
+					"believe": "You should watch X-files"
+				},
+			},
+			"watch": {
+				"films": [
+					{"title": "foo", "year": "2001"},
+					{"title": "bar", "year": "2002"},
+					{"title": "yoo", "year": "2003"},
+				]
 			}
 		}
+	};
+
+	public function new() {
+		search('i.want.it'); // Oh I know you do...
+		search('i.want.to.believe'); // You should watch X-files
+		search('i.want.to.fail'); // null
+		search('i.watch.films'); // [{year: 2001, title: foo}, {year: 2002, title: bar}, {year: 2003, title: yoo}]
+		search('i.watch.films[1]'); // {year: 2002, title: bar}
+		search('i.watch.films[1].year'); // 2002
+	}
+
+	/**
+	 * search in json for the correct value, return null if not
+	 *
+	 * @param search
+	 */
+	function search(search:String) {
 		// var result = JSON.parse(JSON.stringify(json)); //cloning the existing json
 		var result = json; // cloning the existing obj
-		var give = 'i.want.it';
 
-		// now split the give and iterate through keys
-		give.split(".").foreach(function(key) {
-			// result = untyped result[key];
-			trace(key);
+		// now split the searchString and iterate through keys
+		search.split(".").foreach(function(key) {
+			// check for array, if not use the normal way
+			if (key.indexOf('[') != -1) {
+				// get the number in the string `films[1]`
+				var index = Std.parseInt(key.split('[')[1].replace(']', ''));
+				// get the array name `films`
+				key = key.split('[')[0];
+				// get the array
+				var arr = Reflect.field(result, key);
+				// get the index array based on index
+				result = arr[index];
+			} else {
+				result = Reflect.field(result, key);
+			}
 			return true;
 		});
 		trace(result);
+		return result;
 	}
 
 	static public function main() {
