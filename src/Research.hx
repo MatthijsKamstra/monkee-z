c1c5e7f602afda710e0b400c798aadd04404dce2
Matthijs Kamstra <matthijs@fonk.amsterdam>
2021-04-14 12:53:40 +0200

    cleaner

--- src/Research.hx
+++ src/Research.hx
@@ -1,5 +1,6 @@
 package;
 
+import utils.JsonPath;
 import haxe.Json;
 
 using StringTools;
@@ -9,47 +10,16 @@ class Research {
 	public function new() {
 		var content:String = sys.io.File.getContent('docs/data/i.want.it.json');
 
-		search(content, 'i.want.it'); // Oh I know you do...
-		search(content, 'i.want.to.believe'); // You should watch X-files
-		search(content, 'i.want.to.fail'); // null
-		search(content, 'i.watch.films'); // [{year: 2001, title: foo}, {year: 2002, title: bar}, {year: 2003, title: yoo}]
-		search(content, 'i.watch.films[1]'); // {year: 2002, title: bar}
-		search(content, 'i.watch.films[1].year'); // 2002
-		search(content, 'i.watch.films[1000].year'); // null
+		trace(JsonPath.search(content, 'i.want.it')); // Oh I know you do...
+		trace(JsonPath.search(content, 'i.want.to.believe')); // You should watch X-files
+		trace(JsonPath.search(content, 'i.want.to.fail')); // null
+		trace(JsonPath.search(content, 'i.watch.films')); // [{year: 2001, title: foo}, {year: 2002, title: bar}, {year: 2003, title: yoo}]));
+		trace(JsonPath.search(content, 'i.watch.films[1]')); // {year: 2002, title: bar}
+		trace(JsonPath.search(content, 'i.watch.films[1].year')); // 2002
+		trace(JsonPath.search(content, 'i.watch.films[1000].year')); // null
 
-		var arr = search(content, 'i.watch.films');
+		var arr = JsonPath.search(content, 'i.watch.films');
 		trace(arr[2].title); // yoo
-	}
-
-	/**
-	 * search in json for the correct value, return null if not
-	 *
-	 * @param jsonStr
-	 * @param path
-	 */
-	function search(jsonStr:String, path:String) {
-		var result = Json.parse(jsonStr); // cloning the existing json
-		// var result = json; // cloning the existing obj
-
-		// now split the path and iterate through keys
-		path.split(".").foreach(function(key) {
-			// check for array, if not use the normal way
-			if (key.indexOf('[') != -1) {
-				// get the number in the string `films[1]`
-				var index = Std.parseInt(key.split('[')[1].replace(']', ''));
-				// get the array name `films`
-				key = key.split('[')[0];
-				// get the array
-				var arr = Reflect.field(result, key);
-				// get the index array based on index
-				result = arr[index];
-			} else {
-				result = Reflect.field(result, key);
-			}
-			return true;
-		});
-		trace(result);
-		return result;
 	}
 
 	static public function main() {
