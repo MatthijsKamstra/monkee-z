00e1046b6f69141a32a5c1eba82085d7fa466dc7
Matthijs Kamstra <matthijs@fonk.amsterdam>
2021-04-14 11:17:28 +0200

    send json as string

--- src/Research.hx
+++ src/Research.hx
@@ -6,42 +6,29 @@ using StringTools;
 using Lambda;
 
 class Research {
-	// json
-	var json = {
-		"i": {
-			"want": {
-				"it": "Oh I know you do...",
-				"to": {
-					"believe": "You should watch X-files"
-				},
-			},
-			"watch": {
-				"films": [
-					{"title": "foo", "year": "2001"},
-					{"title": "bar", "year": "2002"},
-					{"title": "yoo", "year": "2003"},
-				]
-			}
-		}
-	};
-
 	public function new() {
-		search('i.want.it'); // Oh I know you do...
-		search('i.want.to.believe'); // You should watch X-files
-		search('i.want.to.fail'); // null
-		search('i.watch.films'); // [{year: 2001, title: foo}, {year: 2002, title: bar}, {year: 2003, title: yoo}]
-		search('i.watch.films[1]'); // {year: 2002, title: bar}
-		search('i.watch.films[1].year'); // 2002
+		var content:String = sys.io.File.getContent('docs/data/i.want.it.json');
+
+		search(content, 'i.want.it'); // Oh I know you do...
+		search(content, 'i.want.to.believe'); // You should watch X-files
+		search(content, 'i.want.to.fail'); // null
+		search(content, 'i.watch.films'); // [{year: 2001, title: foo}, {year: 2002, title: bar}, {year: 2003, title: yoo}]
+		search(content, 'i.watch.films[1]'); // {year: 2002, title: bar}
+		search(content, 'i.watch.films[1].year'); // 2002
+
+		var arr = search(content, 'i.watch.films');
+		trace(arr[2].title); // yoo
 	}
 
 	/**
 	 * search in json for the correct value, return null if not
 	 *
+	 * @param jsonStr
 	 * @param search
 	 */
-	function search(search:String) {
-		// var result = JSON.parse(JSON.stringify(json)); //cloning the existing json
-		var result = json; // cloning the existing obj
+	function search(jsonStr:String, search:String) {
+		var result = Json.parse(jsonStr); // cloning the existing json
+		// var result = json; // cloning the existing obj
 
 		// now split the searchString and iterate through keys
 		search.split(".").foreach(function(key) {
