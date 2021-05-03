93c566be3667230fc80c767aa7c5718325ca97ec
Matthijs Kamstra <matthijs@fonk.amsterdam>
2021-05-03 23:12:11 +0200

    fix fetch

--- docs/demo/chain/fetch.js
+++ docs/demo/chain/fetch.js
@@ -23,7 +23,7 @@ var app = new MonkeeChain("#app", {
 });
 
 // Fetch API data
-fetch("../data/json/monkee_load.json")
+fetch("../../data/json/monkee_load.json")
     .then(function (response) {
         return response.json();
     })
