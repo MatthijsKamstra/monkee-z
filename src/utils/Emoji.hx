2705e7c729b1b7f6ce0221c88fa7251119ccf512
Matthijs Kamstra <matthijs@fonk.amsterdam>
2021-05-22 10:04:25 +0200

    more external icons to keep autocomplete in vscode

--- src/utils/Emoji.hx
+++ src/utils/Emoji.hx
@@ -2,4 +2,7 @@ package utils;
 
 class Emoji {
 	public static inline var monkee = '🐵';
+	public static inline var monkeeSeeNo = '🙈';
+	public static inline var monkeeHearNo = '🙉';
+	public static inline var monkeeSpeakNo = '🙊';
 }
