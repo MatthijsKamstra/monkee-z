db8f62e0c0832e32b939e3f4e85740229601e970
Matthijs Kamstra <matthijs@fonk.amsterdam>
2021-05-22 12:09:23 +0200

    MonkeeZ suite test WIP

--- src/MonkeeZ.hx
+++ src/MonkeeZ.hx
@@ -1,6 +1,21 @@
+package;
+
+import js.Browser.*;
+
+using StringTools;
+
+// import externs.MonkeeChain;
+@:expose
+@:keep
 class MonkeeZ {
+	public static var load:MonkeeLoad = new MonkeeLoad();
+
+	// public static var chain:MonkeeChain = new MonkeeChain();
+	public static var route:MonkeeRoute = new MonkeeRoute();
+	public static var bugger:MonkeeBugger = new MonkeeBugger();
+	public static var util:MonkeeUtil = new MonkeeUtil();
+
 	public function new() {
-		trace('MonkeeZ');
 		console.info(App.callIn('MonkeeZ'));
 	}
 
