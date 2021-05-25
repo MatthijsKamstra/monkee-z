8e9cb679f363e043fe958aca76223b1620b4a9b3
Matthijs Kamstra <matthijs@fonk.amsterdam>
2021-05-25 09:19:32 +0200

    just accept we will have no reference to the loaded object (yet)

--- docs/demo/load/event.js
+++ docs/demo/load/event.js
@@ -2,11 +2,12 @@
 
 
 window.addEventListener('onLoadUpdate', function (e) {
-  console.warn(e);
-  // console.warn(e.url);
+  // console.warn(e);
+  console.warn('load update (one element loaded)');
 }, true);
 
 
 window.addEventListener('onLoadReady', function (e) {
-  console.warn(e);
+  // console.warn(e);
+  console.warn('load ready (all object loaded)');
 }, true);
