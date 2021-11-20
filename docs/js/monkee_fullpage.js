4162b9d2a08a00a6eba4170ebe88c7d2007269c0
Matthijs Kamstra <matthijs@fonk.amsterdam>
2021-11-20 14:13:01 +0100

    Release build

--- docs/js/monkee_fullpage.js
+++ docs/js/monkee_fullpage.js
@@ -4,9 +4,8 @@ class MonkeeFullpage {
 		this.scrolltracker = window.document.getElementById("js-scroll-tracker");
 		this.linkArray = [];
 		this.colors = ["#7fdbff","#39cccc","#3d9970","#2ecc40","#01ff70","#ffdc00","#ff851b","#ff4136","#f012be","#b10dc9","#85144b","#ffffff","#dddddd","#aaaaaa","#111111","#001f3f","#0074d9"];
-		this.DEBUG = true;
+		this.DEBUG = false;
 		let _version = "0.0.1";
-		_version = "2021-11-19 10:02:49";
 		$global.console.info("[Monkee-Z]" + " " + ("Fullpage " + "📃") + " - version: " + _version);
 		let _gthis = this;
 		window.document.addEventListener("DOMContentLoaded",function(event) {
@@ -100,5 +99,3 @@ $global.$haxeUID |= 0;
 }
 MonkeeFullpage.main();
 })(typeof window != "undefined" ? window : typeof global != "undefined" ? global : typeof self != "undefined" ? self : this);
-
-//# sourceMappingURL=monkee_fullpage.js.map
