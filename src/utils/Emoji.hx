75620ff29c3d0dfe212427ae29f7ef8a1688f42f
Matthijs Kamstra <matthijs@fonk.amsterdam>
2021-11-20 11:48:06 +0100

    🐞

--- src/utils/Emoji.hx
+++ src/utils/Emoji.hx
@@ -6,5 +6,12 @@ class Emoji {
 	public static inline var monkeeHearNo = '🙉';
 	public static inline var monkeeSpeakNo = '🙊';
 	public static inline var monkeeWrench = '🔧';
+	public static inline var monkeeChain = '🧬';
+	public static inline var monkeeLoad = '🐐';
+	public static inline var monkeeUtil = '🇨🇭';
+	public static inline var monkeeRoute = '🗺';
+	public static inline var monkeeFullpage = '📃';
+	public static inline var monkeeBugger = '🐞';
+	public static inline var monkeeDB = '☢️';
 	public static inline var x = '❌';
 }
